405656a6739390038c190aa5d2f461e2091b9095
Supratimdas20 <supratimdaswork@gmail.com>
2026-03-04 22:24:20 +0530

    Leaning conditionals

---
+++ 07-conditionals.js
@@ -0,0 +1,13 @@
+const playerHealth = 75;
+const hasShield = true;
+const hasSword = false;
+
+if (playerHealth < 100) {
+    console.log("Player is injured");
+}
+if (playerHealth <= 50 && hasShield) {
+    console.log("Player is critically injured and has a shield");
+}
+if (playerHealth <= 30 && hasSword) {
+    console.log("Player is severely injured and has a sword");
+}
