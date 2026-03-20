ef4192bb01601785fb846029c6eb36cd1bedfcfa
Supratimdas20 <supratimdaswork@gmail.com>
2026-03-20 23:20:36 +0530

    Learning

--- practice.js
+++ practice.js
@@ -2,8 +2,10 @@ let city = "Delhi";
 var age = 24;
 const name = "Shaktiman";
 const address = "andjjf"
+let air = "pollution"
 
 console.log(city)
 console.log(age)
 console.log(name)
 console.log(address)
+console.log(air)
