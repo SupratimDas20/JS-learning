09de215cb5fce5ae1cdadb46d2257cc6279f21ef
Supratimdas20 <supratimdaswork@gmail.com>
2026-03-22 23:29:13 +0530

    Learning variables

--- practice.js
+++ practice.js
@@ -1,11 +1,11 @@
 let city = "Delhi";
 var age = 24;
-
+let hero_name = "Shaktiman"
 const address = "andjjf"
 let air = "pollution"
 
 console.log(city)
 console.log(age)
-
+console.log(hero_name)
 console.log(address)
 console.log(air)
