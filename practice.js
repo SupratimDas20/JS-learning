afa3d5417be3014862c689c3e1fef91c4b8dc94e
Supratimdas20 <supratimdaswork@gmail.com>
2026-03-29 23:19:30 +0530

    Learning

--- practice.js
+++ practice.js
@@ -10,23 +10,28 @@ console.log(hero_name)
 console.log(address)
 console.log(air)
 
-let a=10;
-let b=3;
-console.log(a+b);
+let a = 10;
+let b = 3;
+console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
-console.log(5=='5');
-console.log(0==false);
-console.log(5==='5');
-console.log(5===5);
+console.log(5 == '5');
+console.log(0 == false);
+console.log(5 === '5');
+console.log(5 === 5);
 
 let fruits = ["Apple", "Mango", "Banana", "Grapes", "Orange"];
 
 console.log(fruits[0]); // Output: Apple
 console.log(fruits[1]); // Output: Mango
 console.log(fruits[4]); // Output: Orange
+
+function add(a, b) {
+    return a + b;
+}
+
 
 
