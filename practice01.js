3c5e1c18d059bd96e465019148354a6b877c3323
Supratimdas20 <supratimdaswork@gmail.com>
2026-04-13 08:48:43 +0530

    Implementation of the concepts I learned

--- practice01.js
+++ practice01.js
@@ -1,7 +1,56 @@
-// Passing a function as an argument
-const numbers = [3, 1, 4, 1, 5];
-const sorted = numbers.sort(function(a, b) {
-  return a - b;
-});
+// Declaring variables using "var" (older approach).
+var myName = "John";
+console.log(myName);
 
-console.log(sorted); // [1, 1, 3, 4, 5]
+// Declaring variables using "let" [modern approach] variables declared using let their values can be changes in the future.
+let age = 26;
+console.log(age);
+
+// Declaring variables using "const" the value of the variable decelared using const cannot be changes in the future
+const isStudent = true;
+console.log(isStudent);
+
+// isStudent=false;
+// console.log(isStudent);
+
+// Primitive datatypes
+// String 
+let message = "Hello, World!";
+console.log(message);
+let address = '123, Main St';
+console.log(address);
+
+// Numbers
+let count = 10;
+console.log(count);
+let price = 19.192;
+console.log(price);
+let pi = 3.14159;
+console.log(pi);
+
+// Boolean
+let isLoggedIn = true;
+console.log(isLoggedIn);
+let hasPermission = false;
+console.log(hasPermission);
+
+//Null
+let sameValue = null;
+console.log(sameValue);
+
+// Unefined
+let currentScore;
+console.log(currentScore);
+
+// This works fine:
+let temperature = 25;
+console.log(temperature);
+temperature = 30; // Reassignment is ok for "let"
+console.log(temperature);
+
+// This will throw an error
+const birthYear = 1969;
+console.log(birthYear);
+birthYear = 1995;//Error: Assignment to constant variable
+console.log(birthYear);
+
